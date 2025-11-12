de1936e0b0772786af9255abda4d959293e6e1e7
Alban Palandre <a.palandre@la-spa.fr>
2025-11-12 11:10:17 +0100

    Change Annulé

--- src/pages/Transferts.js
+++ src/pages/Transferts.js
@@ -229,7 +229,7 @@ export default function Transferts() {
           <option>En attente</option>
           <option>Confirmé</option>
           <option>Effectué</option>
-          <option>Annulé</option>
+          <option>Adopté</option> {/* <- au lieu de Annulé */}
         </select>
 
         <button className="bg-green-600 hover:bg-green-700 text-white font-semibold px-4 py-2 rounded shadow">
@@ -297,7 +297,7 @@ export default function Transferts() {
                         <option>En attente</option>
                         <option>Confirmé</option>
                         <option>Effectué</option>
-                        <option>Annulé</option>
+                        <option>Adopté</option> {/* <- au lieu de Annulé */}
                       </select>
                     </div>
 
